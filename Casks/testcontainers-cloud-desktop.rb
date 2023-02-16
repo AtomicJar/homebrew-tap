51c94cf0b55087a7f1e32efa25a4010747ea9c8e
Kirill Merkushev <delnariel@proton.me>
2023-02-16 01:31:43 +0100

    Update testcontainers-cloud-desktop from 1.3.23 to 1.3.24

--- Casks/testcontainers-cloud-desktop.rb
+++ Casks/testcontainers-cloud-desktop.rb
@@ -1,31 +1,31 @@
 cask "testcontainers-cloud-desktop" do
-  version "1.3.23"
-  sha256 "7fbb0a37bbe713a843e418e22ddf19a3558fa4d3638e2e34c58bfd4c951ee969"
+  version "1.3.24"
+  sha256 "b97d89a6c1166f94d4b44bd0984f0743207f2057b30fd74b71d6e8564d3899e9"
 
   url "https://app.testcontainers.cloud/download/testcontainers-cloud-desktop_#{version}_darwin_universal.dmg"
   name "Testcontainers Cloud Client"
   desc "Client to connect testcontainers integration tests to Testcontainers Cloud"
   homepage "https://app.testcontainers.cloud/"
-  
-  app "Testcontainers Cloud Desktop.app"
-  
+
   livecheck do
     url "https://app.testcontainers.cloud/api/versions"
     strategy :page_match do |page|
       JSON.parse(page)["latest"]
     end
   end
-  
-  uninstall delete: [
-              "~/Library/Caches/AtomicJar"
+
+  app "Testcontainers Cloud Desktop.app"
+
+  uninstall delete:    [
+              "~/Library/Caches/AtomicJar",
             ],
             launchctl: [
               "testcontainers.cloud.desktop",
             ],
-            quit: "com.atomicjar.cloud.desktop"
-            
+            quit:      "com.atomicjar.cloud.desktop"
+
   zap trash: [
-        "~/Library/Application Support/testcontainers.cloud.desktop",
-        "~/Library/Logs/AtomicJar",
-      ]
+    "~/Library/Application Support/testcontainers.cloud.desktop",
+    "~/Library/Logs/AtomicJar",
+  ]
 end
