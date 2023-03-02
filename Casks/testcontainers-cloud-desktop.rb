3f4cfb0de17efaeafdebe026f83e4b79fbab0089
Kirill Merkushev <delnariel@proton.me>
2023-03-02 20:16:25 +0100

    Don't cleanup services every upgrade

--- Casks/testcontainers-cloud-desktop.rb
+++ Casks/testcontainers-cloud-desktop.rb
@@ -2,7 +2,8 @@ cask "testcontainers-cloud-desktop" do
   version "1.3.25"
   sha256 "4ac29609f8549b54579387f661fb239570049ea4296346652ff245ccf698b528"
 
-  url "https://app.testcontainers.cloud/download/testcontainers-cloud-desktop_#{version}_darwin_universal.dmg"
+  url "https://app.testcontainers.cloud/download/testcontainers-cloud-desktop_#{version}_darwin_universal.dmg",
+      user_agent: "brew-cask"
   name "Testcontainers Cloud Client"
   desc "Client to connect testcontainers integration tests to Testcontainers Cloud"
   homepage "https://app.testcontainers.cloud/"
@@ -16,16 +17,23 @@ cask "testcontainers-cloud-desktop" do
 
   app "Testcontainers Cloud Desktop.app"
 
-  uninstall delete:    [
+  postflight do
+    system_command "open",
+                   args: ["#{appdir}/Testcontainers Cloud Desktop.app"]
+  end
+
+  uninstall delete: [
               "~/Library/Caches/AtomicJar",
             ],
-            launchctl: [
-              "testcontainers.cloud.desktop",
-            ],
-            quit:      "com.atomicjar.cloud.desktop"
+            quit:   [
+              "com.atomicjar.cloud.desktop",
+            ]
 
-  zap trash: [
-    "~/Library/Application Support/testcontainers.cloud.desktop",
-    "~/Library/Logs/AtomicJar",
-  ]
+  zap trash:  [
+        "~/Library/Application Support/testcontainers.cloud.desktop",
+        "~/Library/Logs/AtomicJar",
+      ],
+      delete: [
+        "~/Library/LaunchAgents/testcontainers.cloud.desktop.plist",
+      ]
 end
